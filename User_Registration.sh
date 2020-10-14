5268ffd91bf28986183723189458ce3916caebe1
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-10-14 22:23:09 +0530

    Added : Valid User EmailId

--- User_Registration.sh
+++ User_Registration.sh
@@ -4,7 +4,7 @@ echo "Welcome to User Registration"
 
 read -p "Enter the name : " Name
 
-pat='^[a-zA-Z]{3,}[ ]{1}[a-zA-Z]{3,}$'
+pat='^[a-zA-Z]{3}[.]{1}[a-zA-Z]{3}(@)[a-zA-Z]{2}[.][a-zA-Z]{2}[.][a-zA-Z]{2}$'
 
 if [[ $Name =~ $pat ]]
 then
